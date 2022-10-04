d3fcc568aeac1aa10e0e5bc765f10e6b6f872c0d
xloem <0xloem@gmail.com>
2022-10-04 08:01:34 -0400

    Detect presence of lua

--- scripts/lua/install_dependencies.sh
+++ scripts/lua/install_dependencies.sh
@@ -1,5 +1,11 @@
 #!env bash
 
+if ! luarocks config --lua-ver | grep '5\.[3456789]'
+then
+  echo 'please first install lua >=5.3 with luarocks'
+  exit -1
+fi
+
 luarocks install --local lfs
 luarocks install --local penlight
 luarocks install --local lua-toml
